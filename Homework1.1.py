f486b0a004e3835c863fe9a0e31d540d60880dc0
mlgiaime <melissa.giaime@gmail.com>
2016-06-28 19:21:08 -0700

    Homework 1.1: Print 3 statements.

---
+++ Homework1.1.py
@@ -0,0 +1,3 @@
+# Write a program that displays "Welcome to Python", "Welcome to Computer Science", and "Programming is fun"
+
+print("Welcome to Python. ", "Welcome to Computer Science. ", "Programming is Fun.")
